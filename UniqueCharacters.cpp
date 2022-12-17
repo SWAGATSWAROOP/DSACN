2c4abcc440c21ccc3f75d9c6f391af4ccd78f2ff
Swagat Swaroop Parida <swagatswaroop@gmail.com>
2022-12-17 11:28:34 +0530

    Priority Queue

---
+++ UniqueCharacters.cpp
@@ -0,0 +1,33 @@
+// xtract Unique characters
+// Send Feedback
+// Given a string S, you need to remove all the duplicates. That means, the output string should contain each character only once. The respective order of characters should remain same, as in the input string.
+// Input format:
+// The first and only line of input contains a string, that denotes the value of S.
+// Output format :
+// The first and only line of output contains the updated string, as described in the task.
+// Constraints :
+// 0 <= Length of S <= 10^8
+// Time Limit: 1 sec
+// Sample Input 1 :
+// ababacd
+// Sample Output 1 :
+// abcd
+// Sample Input 2 :
+// abcde
+// Sample Output 2 :
+// abcde
+
+#include <unordered_map>
+string uniqueChar(string str) {
+	// Write your code here
+	unordered_map<char,int> map;
+	string a;
+	for(int i=0;i<str.size();i++){
+		if(map.count(str[i])>0){
+			continue;
+		}
+		map[str[i]] = 1;
+		a += str[i];
+	}
+	return a;
+}
